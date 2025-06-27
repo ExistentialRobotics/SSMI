65c17dce28074867241ea3ba9ddfd7eaa7cd8c0d
Tianji <tit006@ucsd.edu>
2025-06-27 01:00:56 -0700

    add library

--- ssmi_mapping/scripts/dynamic_tf.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from ssmi_mapping.dynamic_tf import main
-# Main entry point for the dynamic TF node
-if __name__ == '__main__':
-    main()
--- ssmi_mapping/scripts/semantic_sensor_node.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from ssmi_mapping.semantic_sensor_node import main
-# Main entry point for the semantic sensor node
-if __name__ == '__main__':
-    main()
